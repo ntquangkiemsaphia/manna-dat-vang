68cf08179d8c540cf37b6e5c2c13f3c8711534bf
Minh Chu Nguyen <92323761+chuismee@users.noreply.github.com>
2026-04-15 08:28:04 +0700

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,13 +4,20 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+
   test: {
     environment: "jsdom",
     globals: true,
     setupFiles: ["./src/test/setup.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"],
   },
+
   resolve: {
     alias: { "@": path.resolve(__dirname, "./src") },
+  },
+
+  server: {
+    host: true,
+    allowedHosts: ["vuakiem.com"],
   },
 });
