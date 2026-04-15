8b856d961e30b484437d62fe16d8a58a996f7774
Minh Chu Nguyen <92323761+chuismee@users.noreply.github.com>
2026-04-15 08:34:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,7 @@ export default defineConfig(({ mode }) => ({
     hmr: {
       overlay: false,
     },
+    allowedHosts: ["vuakiem.com"],
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
